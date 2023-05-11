49c21fefbe1fec093ea6f2fb2ca496ba97c7915f
PRIYANKA-FLORA <124249400+PRIYANKA-FLORA@users.noreply.github.com>
2023-05-11 17:33:16 +0530

    video slider

--- js/custom.js
+++ js/custom.js
@@ -20,5 +20,23 @@ $(document).ready(function(){
             }
         }
     });
+
+
+/* Video Slider */
+    $('.owl-carousel').owlCarousel({
+        stagePadding: 200,
+        loop:true,
+        items:1,
+        lazyLoad: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true
+    })
+    
        
 });
+
+
+
+
